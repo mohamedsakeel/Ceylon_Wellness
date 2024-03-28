abf3a13f7514fbe49b137efb6b155023d4ded4f2
Sakeel <mohamedsakeel@outlook.com>
2024-03-28 21:26:15 +0530

    External Login
    Setted up google and facebook login secrets and ran migration

---
+++ CeylonWellness.Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CeylonWellness.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeylonWellness.Data
+{
+    public  class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- CeylonWellness.Web/Program.cs
+++ CeylonWellness.Web/Program.cs
@@ -1,7 +1,35 @@
+using CeylonWellness.Data;
+using CeylonWellness.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Owin.Security.Google;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>();
+
+#region External Authentication
+
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = "306909278130-13sttfr5ujqm9h047ic2h6rlmfu8nne9.apps.googleusercontent.com";
+        options.ClientSecret = "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";
+    })
+    .AddFacebook(options =>
+    {
+        options.AppId = "your_facebook_app_id";
+        options.AppSecret = "your_facebook_app_secret";
+    });
+
+#endregion
 
 var app = builder.Build();
 
@@ -13,6 +41,11 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+#region Google/Facebook Login
+
+
+
+#endregion
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
